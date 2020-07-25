ae06c97e317e38d6264d3cafa40890018df17441
cloet <mathias.cloet@hotmail.com>
2020-07-25 09:32:16 +0200

    init

--- veilingservice/Migrations/20200725073145_auctiostatus.Designer.cs
+++ veilingservice/Migrations/20200725073145_auctiostatus.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using veilingservice.Data;
@@ -9,9 +10,10 @@ using veilingservice.Data;
 namespace veilingservice.Migrations
 {
     [DbContext(typeof(VeilingContext))]
-    partial class AuctionContextModelSnapshot : ModelSnapshot
+    [Migration("20200725073145_auctiostatus")]
+    partial class auctiostatus
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ veilingservice/Migrations/20200725073145_auctiostatus.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace veilingservice.Migrations
+{
+    public partial class auctiostatus : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "Auction",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Auction");
+        }
+    }
+}
